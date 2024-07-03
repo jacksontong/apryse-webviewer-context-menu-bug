4ebf75ed1a8959e321fb95d3e5004fb47e411871
Jackson Tong <tongtoan2704@gmail.com>
2024-07-03 13:41:32 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dicta
+      dolore, sunt ut culpa animi! Ab, esse. Dicta perspiciatis nesciunt, est
+      perferendis vitae vel atque rerum facere, consequatur numquam alias!
+    </div>
+  );
+}
+
+export default App;
